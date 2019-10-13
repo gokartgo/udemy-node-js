a4bd153cb29a920a1a7c285cd6f82e1a7e40f01b
Kitcharnon limtongsittikun <kitcharnon@gmail.com>
2019-10-13 21:41:46 +0700

    course mongodb finish

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,12 +3,11 @@ const Product = require('../models/product');
 const getProducts = (req, res, next) => {
     Product.fetchAll()
         .then((products) => {
-            res.render('shop/product-list',
-                {
-                    prods: products,
-                    pageTitle: 'All Products',
-                    path: '/products',
-                });
+            res.render('shop/product-list', {
+                prods: products,
+                pageTitle: 'All Products',
+                path: '/products',
+            });
         })
         .catch(err => {
             console.log(err);
@@ -29,18 +28,14 @@ const getIndex = (req, res, next) => {
 
 
 const getCart = (req, res, next) => {
-    req.user.getCart()
-        .then(cart => {
-            return cart.getProducts()
-                .then(products => {
-                    console.log('product', products)
-                    res.render('shop/cart',
-                        {
-                            path: '/cart',
-                            pageTitle: 'Your Cart',
-                            products
-                        });
-                })
+    req.user
+        .getCart()
+        .then(products => {
+            return res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products
+            })
         })
         .catch(err => {
             console.log(err);
@@ -68,26 +63,8 @@ const getCart = (req, res, next) => {
 
 const postOrder = (req, res, next) => {
     let fetchedCart;
-    req.user.getCart()
-        .then(cart => {
-            fetchedCart = cart;
-            return cart.getProducts();
-        })
-        .then(products => {
-            return req.user.createOrder()
-                .then(order => {
-                    return order.addProducts(products.map(product => {
-                        product.orderItem = { quantity: product.cartItem.quantity };
-                        return product;
-                    }))
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        })
-        .then(result => {
-            fetchedCart.setProducts(null);
-        })
+    req.user
+        .addOrder()
         .then(result => {
             res.redirect('/orders');
         })
@@ -97,7 +74,7 @@ const postOrder = (req, res, next) => {
 }
 
 const getOrders = (req, res, next) => {
-    req.user.getOrders({ include: ['products'] })
+    req.user.getOrders()
         .then(orders => {
             console.log('getOrder orders', orders)
             res.render('shop/orders', {
@@ -112,7 +89,9 @@ const getOrders = (req, res, next) => {
 }
 
 const getProduct = (req, res, next) => {
-    const { productId = '' } = req.params;
+    const {
+        productId = ''
+    } = req.params;
     // Product.findById(productId, product => {
     //     res.render('shop/product-detail', { product, pageTitle: product.title, path: '/products' })
     // });'
@@ -130,7 +109,11 @@ const getProduct = (req, res, next) => {
     //     })
     Product.findByPk(productId)
         .then((product) => {
-            res.render('shop/product-detail', { product, pageTitle: product.title, path: '/products' })
+            res.render('shop/product-detail', {
+                product,
+                pageTitle: product.title,
+                path: '/products'
+            })
         })
         .catch(err => {
             console.log(err)
@@ -144,51 +127,13 @@ const postCart = (req, res, next) => {
         return req.user.addToCart(product);
     }).then(result => {
         console.log(result)
+        res.redirect('/cart');
     })
-    // let fetchCart;
-    // let newQuantity = 1;
-    // req.user.getCart()
-    //     .then(cart => {
-    //         console.log('cart', cart);
-    //         fetchCart = cart;
-    //         return cart.getProducts({ where: { id: prodId } });
-    //     })
-    //     .then(products => {
-    //         console.log('products', products)
-    //         let product;
-    //         if (products.length > 0) {
-    //             product = products[0];
-    //         }
-    //         if (product) {
-    //             const oldQuantity = product.cartItem.quantity;
-    //             newQuantity = oldQuantity + 1;
-    //             return product
-    //         }
-    //         return Product.findByPk(prodId)
-    //     })
-    //     .then(product => {
-    //         return fetchCart.addProduct(product, {
-    //             through: {
-    //                 quantity: newQuantity
-    //             }
-    //         });
-    //     })
-    //     .then(() => {
-    //         res.redirect('/cart');
-    //     })
 }
 
 const postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    req.user.getCart()
-        .then(cart => {
-            return cart.getProducts({ where: { id: prodId } });
-        })
-        .then(products => {
-            console.log('postCartDeleteProduct products', products);
-            const product = products[0];
-            return product.cartItem.destroy();
-        })
+    req.user.deleteItemFromCart(prodId)
         .then(result => {
             res.redirect('/cart');
         })
--- routes/shop.js
+++ routes/shop.js
@@ -18,14 +18,14 @@ router.get('/products', shopController.getProducts);
 
 // router.get('/products/:productId', shopController.getProduct) // ถ้าเอา /products/delete ไว้ข้างล่างตาม comment จะไม่ถูกทำงานแก้โดยไว้ข้างบนเพราะ code จะทำงานจากบนลงล่าง
 
-// router.get('/cart', shopController.getCart);
+router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-// router.post('/create-order', shopController.postOrder);
+router.post('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
