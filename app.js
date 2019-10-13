b2f811113450badeedbe71b456d87c1dce8b6c91
Kitcharnon limtongsittikun <kitcharnon@gmail.com>
2019-10-13 21:50:11 +0700

    start mongoose

--- app.js
+++ app.js
@@ -3,35 +3,22 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user')
 const app = express();
 
-// for handle
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-// }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// const rqListener = (req, res) => {
-//     console.log(routes.someText)
-//     routes.handler(req, res)
-// }
-
-// app.use((req, res, next) => {
-//     console.log('In the middleware!');
-//     next(); // Allows the request to continue to next middieware in line
-// });
-
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
@@ -46,10 +33,6 @@ app.use((req, res, next) => {
 app.use('/admin', adminRoutes.router);
 app.use(shopRoutes);
 
-
-// const server = http.createServer(app);
-
-// server.listen(9000);
 console.log(__dirname);
 app.use(errorController.get404Page);
 
